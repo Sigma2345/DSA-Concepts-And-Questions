b07099e23da13f9aa184a4146a2c0cdb63b690df
Sigma2345 <mudit1978mars@gmail.com>
2022-10-13 00:03:41 +0530

    daily challenge 12 oct 2022

---
+++ leetcode/Daily_Challenge/12_10_2022_LargestPerimeterTraingle_976.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/largest-perimeter-triangle/
+
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(), nums.end()); 
+        // largest perimeter possibe is from 3 subsequent elements
+        for(int i = nums.size()-3;  i>=0 ; i--){
+            if(nums[i] + nums[i+1] > nums[i+2]){
+                return nums[i] + nums[i+1] + nums[i+2] ; 
+            }
+        }
+        return 0 ;
+    }
+};
